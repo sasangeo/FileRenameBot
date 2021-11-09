ec86e67620ef4630f8d6ea2e1f9ca6f3794fcac8
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-11-10 04:47:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config(object):
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
-    # proxy for accessing youtube-dl in GeoRestricted Areas
+    
     
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     # https://t.me/hevcbay/951
